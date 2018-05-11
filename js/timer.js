fb32ea52feedfbb3d935f4f743bc260e52b6d488
cdilga <c.dilger1@gmail.com>
2018-05-11 10:05:34 +1000

    Anniversary update

--- js/timer.js
+++ js/timer.js
@@ -3,13 +3,13 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('May 12, 2018 12:00:00').getTime(),
+let countDown = new Date('May 11, 2017 14:30:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
-          distance = countDown - now;
+        distance = now - countDown;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+       document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
