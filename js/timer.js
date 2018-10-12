9f70a7bff1f338891d182f81bbd5e05b3b59e18c
cdilga <c.dilger1@gmail.com>
2018-10-11 23:25:03 -0400

    Update to pre-birthday

--- js/timer.js
+++ js/timer.js
@@ -3,11 +3,11 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('May 11, 2017 14:30:00').getTime(),
+let countDown = new Date('October 16, 2018 14:30:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
-        distance = now - countDown;
+        distance = countDown - now;
 
        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
